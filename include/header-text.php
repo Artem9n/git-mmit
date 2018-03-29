aeab4cd55191b1ee09f62c199b4bd8cb23464b4a
fait <Artem9n24@yandex.ru>
2018-03-29 18:07:58 +0800

    init

---
+++ include/header-text.php
@@ -0,0 +1,13 @@
+<span style="color: #000000;">Эксперт в области кровельных </span><br>
+<span style="color: #000000;">
+и фасадных систем</span>
+<?$APPLICATION->IncludeComponent(
+    "bitrix:main.include",
+    "",
+    Array(
+        "AREA_FILE_RECURSIVE" => "Y",
+        "AREA_FILE_SHOW" => "sect",
+        "AREA_FILE_SUFFIX" => "inc",
+        "EDIT_TEMPLATE" => ""
+    )
+);?>
